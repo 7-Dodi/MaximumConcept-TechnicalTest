7243b879f103847f0d5f255a2afdda2c073c934a
7-Dodi <douglassilvaaraujo78@gmail.com>
2025-05-31 21:51:53 -0300

    feat: Criando página inicial

---
+++ frontend/src/pages/Regsiter-Page/index.tsx
@@ -0,0 +1,14 @@
+// **Importações
+import * as React from "react";
+
+// **Styles
+import "./style.css";
+
+export const RegisterPage = () => {
+  return (
+    <div className="register-page-container">
+      <h1>Register Page</h1>
+      <p>This is the register page.</p>
+    </div>
+  );
+}
