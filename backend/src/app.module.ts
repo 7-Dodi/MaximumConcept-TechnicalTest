c667b42c7258641843cdbdb222ebb84ba113bd90
7-Dodi <douglassilvaaraujo78@gmail.com>
2025-06-05 09:52:02 -0300

    refactor: Atualizando modulos do sistema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
+import { RequestsModule } from './requests/requests.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    DbModule,
+    RequestsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
