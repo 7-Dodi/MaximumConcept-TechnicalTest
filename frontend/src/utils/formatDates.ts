a0ede972686aa4595bffdef2dc441d4a4dfa4721
7-Dodi <douglassilvaaraujo78@gmail.com>
2025-06-06 10:36:05 -0300

    refactor: Criando nova função de retorno e adaptação de datas

--- frontend/src/utils/formatDates.ts
+++ frontend/src/utils/formatDates.ts
@@ -7,3 +7,16 @@ export const formatDate = (date: Date): string => {
     day: "2-digit",
   });
 };
+
+export const formatDateTime = (date: Date): string => {
+  const d = new Date(date);
+
+  const hours = String(d.getHours()).padStart(2, "0");
+  const minutes = String(d.getMinutes()).padStart(2, "0");
+
+  const day = String(d.getDate()).padStart(2, "0");
+  const month = String(d.getMonth() + 1).padStart(2, "0");
+  const year = d.getFullYear();
+
+  return `Atualizado às: ${hours}:${minutes}, de ${day}/${month}/${year}`;
+};
