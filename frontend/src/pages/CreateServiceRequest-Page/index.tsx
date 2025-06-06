cff5b45f6880779eaf7e34350ae5e231a6d17319
7-Dodi <douglassilvaaraujo78@gmail.com>
2025-06-06 10:30:39 -0300

    refactor: Atualizando a página de criação de serviços para a integração com api do backend

--- frontend/src/pages/CreateServiceRequest-Page/index.tsx
+++ frontend/src/pages/CreateServiceRequest-Page/index.tsx
@@ -9,7 +9,10 @@ import "./style.css";
 
 // **Types
 import { typeOptions } from "../../types/selectFilters";
-import type { CreateServiceRequest, ServiceRequest } from "../../types/serviceRequest";
+import type {
+  CreateServiceRequest,
+  RequestStatus,
+} from "../../types/serviceRequest";
 
 // **Validação
 import { createServiceSchema } from "../../validations/service/create";
@@ -17,11 +20,19 @@ import { createServiceSchema } from "../../validations/service/create";
 // **Components
 import { BackPage } from "../../components/BackPage-Component";
 
-// **Mocks
-import { mockUsers } from "../../types/mocks/MocksData";
+// **Contexto
+import { useUser } from "../../contexts/User-contexto";
+import { useAuth } from "../../contexts/Auth-context";
+import { toast } from "react-toastify";
+
+// **Api
+import * as requestService from "../../services/request-service";
 
 export const CreateServiceRequestPage = () => {
   const navigate = useNavigate();
+
+  const { user } = useUser();
+  const { token } = useAuth();
 
   const {
     register,
@@ -32,21 +43,30 @@ export const CreateServiceRequestPage = () => {
     resolver: yupResolver(createServiceSchema),
   });
 
-  const onSubmit: SubmitHandler<CreateServiceRequest> = (data) => {
+  const onSubmit: SubmitHandler<CreateServiceRequest> = async (data) => {
     try {
-      const payload: Omit<ServiceRequest, "id" | "createdAt" | "updatedAt"> = {
+      if (!token || !user) return;
+
+      toast.loading("Criando solicitação...");
+
+      const payload: CreateServiceRequest & { status: RequestStatus } = {
         ...data,
-        requester: mockUsers[0],
         status: "pendente",
       };
-      console.log(payload);
+
+      await requestService.createServiceRequest(token, payload);
 
       reset();
-      navigate("/dashboard/" + payload.requester.type);
+      navigate("/dashboard/" + user.type);
+
+      toast.dismiss();
+      toast.success("Solicitação criada com sucesso!");
     } catch (error) {
+      toast.dismiss();
+      toast.error("Error ao criar solicitação!");
       console.error("Erro ao enviar o formulário:", error);
     }
-  }
+  };
 
   return (
     <div className="create-service-request-container container">
@@ -60,11 +80,17 @@ export const CreateServiceRequestPage = () => {
         </div>
 
         <div className="create-service-request-form">
-          <form className="create-service-form" onSubmit={handleSubmit(onSubmit)}>
+          <form
+            className="create-service-form"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <div className="create-service-form-inputs">
               <label className="create-service-form-label">
                 <span className="create-service-form-span">Tipo:</span>
-                <select className="create-service-form-input" {...register("type")}>
+                <select
+                  className="create-service-form-input"
+                  {...register("type")}
+                >
                   {typeOptions.map((option) => (
                     <option
                       className="create-service-form-option"
@@ -89,7 +115,9 @@ export const CreateServiceRequestPage = () => {
                   {...register("description")}
                 />
                 {errors.description && (
-                  <span className="form-error">{errors.description.message}</span>
+                  <span className="form-error">
+                    {errors.description.message}
+                  </span>
                 )}
               </label>
 
